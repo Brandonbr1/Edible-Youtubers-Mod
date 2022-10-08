6b7d8180c410df967d6cf370673107d68a08bf82
Java sauce <mohammedbrandon204@gmail.com>
2022-10-08 16:37:14 -0400

    recipes

--- src/main/java/com/jerios/yteat/init/FoodInit.java
+++ src/main/java/com/jerios/yteat/init/FoodInit.java
@@ -7,49 +7,49 @@ import net.minecraft.potion.Effects;
 public class FoodInit {
 	
 	public static final Food GIRLYGAMERFOOD = new Food.Builder()
-			.effect(() -> new EffectInstance(Effects.REGENERATION, 5, 2000), 0.5f)
+			.effect(() -> new EffectInstance(Effects.REGENERATION, 10, 2000), 0.5f)
 			.fastToEat().hunger(2).saturation(0.2f)
 			.setAlwaysEdible().build();
 	
 	public static final Food DANTDMFOOD = new Food.Builder()
-			.effect(() -> new EffectInstance(Effects.HASTE, 6, 2000), 0.5f)
+			.effect(() -> new EffectInstance(Effects.HASTE, 10, 2000), 0.5f)
 			.fastToEat().hunger(2).saturation(0.2f)
 			.setAlwaysEdible().build();
 	
 	public static final Food SKYDOESMINECRAFTBUILDER = new Food.Builder()
-			.effect(() -> new EffectInstance(Effects.SPEED, 6, 2000), 0.5f)
+			.effect(() -> new EffectInstance(Effects.SPEED, 10, 2000), 0.5f)
 			.fastToEat().hunger(2).saturation(0.2f)
 			.setAlwaysEdible().build();
 	
 	public static final Food VOXBUILDER = new Food.Builder()
-			.effect(() -> new EffectInstance(Effects.JUMP_BOOST, 5, 2000), 0.5f)
+			.effect(() -> new EffectInstance(Effects.JUMP_BOOST, 10, 2000), 0.5f)
 			.fastToEat().hunger(2).saturation(0.2f)
 			.setAlwaysEdible().build();
 	
 	
 	public static final Food SSUNDEEATBUILDER = new Food.Builder()
-			.effect(() -> new EffectInstance(Effects.NAUSEA, 5, 2000), 0.5f)
+			.effect(() -> new EffectInstance(Effects.NAUSEA, 10, 2000), 0.5f)
 			.fastToEat().hunger(2).saturation(0.2f)
 			.setAlwaysEdible().build();
 	
 	public static final Food PRESTONBUILDER = new Food.Builder()
-			.effect(() -> new EffectInstance(Effects.FIRE_RESISTANCE, 1, 2000), 0.5f)
+			.effect(() -> new EffectInstance(Effects.FIRE_RESISTANCE, 10, 2000), 0.5f)
 			.fastToEat().hunger(2).saturation(0.2f)
 			.setAlwaysEdible().build();
 	
 	public static final Food COLBANATORBUILDER = new Food.Builder()
-			.effect(() -> new EffectInstance(Effects.STRENGTH, 6, 2000), 0.5f)
+			.effect(() -> new EffectInstance(Effects.STRENGTH, 10, 2000), 0.5f)
 			.fastToEat().hunger(2).saturation(0.2f)
 			.setAlwaysEdible().build();
 	
 	public static final Food POPULARMMOS = new Food.Builder()
-			.effect(() -> new EffectInstance(Effects.RESISTANCE, 6, 2000), 0.5f)
-			.effect(() -> new EffectInstance(Effects.STRENGTH, 8, 2000), 0.5f)
+			.effect(() -> new EffectInstance(Effects.RESISTANCE, 10, 2000), 0.5f)
+			.effect(() -> new EffectInstance(Effects.STRENGTH, 15, 2000), 0.5f)
 			.fastToEat().hunger(2).saturation(0.2f)
 			.setAlwaysEdible().build();
 	
 	public static final Food DREAM = new Food.Builder()
-			.effect(() -> new EffectInstance(Effects.LUCK, 20, 2000), 0.5f)
+			.effect(() -> new EffectInstance(Effects.LUCK, 10, 2000), 0.5f)
 			.effect(() -> new EffectInstance(Effects.DOLPHINS_GRACE, 8, 2000), 0.5f)
 			.fastToEat().hunger(2).saturation(0.2f)
 			.setAlwaysEdible().build();
